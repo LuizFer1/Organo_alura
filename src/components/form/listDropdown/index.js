fb6db3936d5305bfd92372d93694c32b6b637d86
Luiz <luisferndantas@outlook.com>
2023-08-10 20:48:34 -0300

    fix: option listdropdown

--- src/components/form/listDropdown/index.js
+++ src/components/form/listDropdown/index.js
@@ -9,6 +9,7 @@ function SquadLists(props) {
                 {props.label}
             </label>
             <select onChange={e => props.valorAlterado(e.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
